9f1b8f134fc1f87f19b3a5556b470d55db4dc6e1
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-08-29 11:01:35 -0400

    update fields

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -30,9 +30,16 @@ function Header(props) {
 
           <h2>About me</h2>
           <p>
-            Welcome to Lindsey Martin's portfolio page built with React. Here,
-            you can find a small sample of the projects she has worked on over
-            the course of the program.
+          My name is Dennis Luciano, I worked as a Spanish teacher for the last 20 years. 
+          It has been a passion to work with students and see them grow. Teaching from College, 
+          High School and Middle School has given me an appreciation on how technology influences 
+          and impacts our lives. I have a masters of technology education which has given me the 
+          drive to pursue a new career in computer web development. On a personal note, my favorite 
+          thing to do is to travel across the great country of the United States. Traveling with my 
+          eight year old son has been so rewarding as he gets to see different sites and 
+          cultures across the country. Traveling in our family RV has given me a new appreciation 
+          of lifestyle design. Traveling has given me the desire to work remotely. 
+          This would allow me and my family to travel more to see different sites in our great nation.
           </p>
         </div>
       </section>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,37 +5,34 @@ import weatherDashImage from "../../assets/small/Weather_Dash.png";
 import noteTakerImage from "../../assets/small/NoteTaker.png";
 import twilightImage from "../../assets/small/Twilight_Ristorante.png";
 import flowerImage from "../../assets/small/Sweet-Pea-Flowers.png";
-import sphereImage from "../../assets/small/Sphere.png";
+import weatherImage from "../../assets/small/weather-site.jpg";
 
 function Portfolio() {
   return (
     <div>
       <section id="work" className="jobs">
         <div className="flex-row">
-          <h2 className="section-title secondary-border">Work</h2>
+          <h2 className="section-title secondary-border">Sample Work</h2>
         </div>
 
         <div className="job">
           <div className="job-info">
             <div className="job-img">
-              <a href="https://lemming97.github.io/Sweet-Pea-Flowers/">
+              <a href="https://dencoding2023.github.io/Weather-Dashboard//">
                 {" "}
                 <img
-                  src={sphereImage}
+                  src={weatherImage}
                   className="my-2"
                   style={{ width: "100%" }}
-                  alt="Sweet-Pea-Flowers"
+                  alt="Weather-Dashboard"
                 />
               </a>
             </div>
             <div className="job-text">
-              <h4>Sorcerer's Sphere</h4>
+              <h4>Weather-Dashboard</h4>
               <p>
-                A MERN application that lets the user log in and create an
-                account. The user can receive daily affirmations. The user also
-                has the ability to ask questions to receive an answer. The user
-                also can receive a lucky number if they want. The user can
-                donate money to the website if they want to.
+              This is a website that lets you know the weather in your area. 
+              You are able to get the temperature, humidity and weather description.
               </p>
             </div>
           </div>
